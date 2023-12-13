d2b9bc79b8df07837264848f657b47305492fd3d
noramiao <sun.xiaopu84@gmail.com>
2023-12-13 18:32:06 +1000

    create selection menu def

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print ("Welcome to the stock tracker!")
 
+def selection_menu():
+    print("1. Enter 1 to check current stock number")
+    print("2. Enter 2 to report new arrival stock number")
+    print("3. Enter 3 to report stock on hold number")
+    print("4. Enter 4 to report return stock number")
+    print("5. Enter 5 to exit")
+    choice = input("Enter your selection: ")
+    return choice
+
