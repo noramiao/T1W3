a73a38c0b2297614c5c0db94af75d65907811958
noramiao <sun.xiaopu84@gmail.com>
2023-12-13 18:33:59 +1000

    add loop for user input

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ def selection_menu():
     choice = input("Enter your selection: ")
     return choice
 
+users_selection = ""
+
+while users_selection!= "5":
+    users_selection = selection_menu()
+    if (users_selection == "1"):
+        print ("check stock level")
+    elif (users_selection == "2"):
+        print("add new arrival stock")
+    elif (users_selection == "3"):
+        print ("report stock on hold")
+    elif (users_selection == "4"):
+        print ("report return stock")
+    elif (users_selection == "5"):
+        continue
+    else:
+        print("Invalid Input")
