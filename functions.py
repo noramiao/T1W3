744c7bd80e0c7c6eed403ad195ad2143f4a8c515
noramiao <sun.xiaopu84@gmail.com>
2023-12-13 18:35:27 +1000

    create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+def check_stock():
+    print("current stock level is")
+
+def add_new_stock():
+    print ("add new stock")
+
+def stock_on_hold():
+    print("stock on hold")
+
+def return_stock():
+    print("return stock")
